e4927dcd1e906e70a0f213d75251b526a367dc40
Mansoor <shaikmansoor1200@gmail.com>
2024-12-05 20:15:47 +0530

    app, server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const mongoUri = process.env.MONGO_URI;
 console.log('Mongo URI:', process.env.MONGO_URI);
 
 // Connect to MongoDB
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUri)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
 
