695e1205606ccc4d8282d35ee7a5f249d0e5dd4c
Mansoor <shaikmansoor1200@gmail.com>
2024-12-15 09:58:55 +0530

    changes

--- server.js
+++ server.js
@@ -1,31 +1,48 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User.js');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 const port = process.env.PORT || 4041;
+const mongoUri = process.env.MONGO_URI;
 
-// Custom middleware to log CORS details
-app.use((req, res, next) => {
-    console.log(`Incoming Request: ${req.method} ${req.url}`);
-    console.log(`Origin: ${req.headers.origin}`);
-    next();
-});
+console.log('Mongo URI:', mongoUri);
 
-// Enable CORS and log CORS status
-const corsOptions = {
-    origin: (origin, callback) => {
-        console.log(`Checking CORS for Origin: ${origin}`);
-        callback(null, true); // Allow all origins; adjust as needed
-    },
-};
-app.use(cors(corsOptions));
+// Connect to MongoDB
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
 
 app.get('/', (req, res) => {
-    res.send('hi this is backend home page');
+    res.send('Hello World! this is updated on 10:40 pm');
 });
 
 app.get('/api/message', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
+});
+
+app.get('/api/db-status', (req, res) => {
+    const status = mongoose.connection.readyState === 1 ? 'Connected to MongoDB' : 'Not connected to MongoDB';
+    res.json({ status });
+});
+
+app.post('/api/save-user', (req, res) => {
+    console.log('Received user data:', req.body);
+    const { name, email } = req.body;
+    const user = new User({ name, email });
+    user.save()
+        .then(() => {
+            console.log('User saved successfully');
+            res.json({ message: 'User saved successfully' });
+        })
+        .catch(err => {
+            console.error('Error saving user:', err);
+            res.status(500).json({ error: 'Error saving user', details: err.message });
+        });
 });
 
 app.listen(port, () => {
