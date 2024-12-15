2d693ea7e92798d9ce93c8ea6477f95cf1d21925
TruleeInnovate <185590719+TruleeInnovate12@users.noreply.github.com>
2024-12-15 18:26:42 +0530

    Update fetchMasterData.js

--- frontend/src/utils/fetchMasterData.js
+++ frontend/src/utils/fetchMasterData.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-const backendUrl = process.env.NODE_ENV === 'production'
-? 'https://basic-backend-001-fadbheefgmdffzd4.uaenorth-01.azurewebsites.net'
-: 'http://localhost:4041';
+// const backendUrl = process.env.NODE_ENV === 'production'
+// ? 'https://basic-backend-001-fadbheefgmdffzd4.uaenorth-01.azurewebsites.net'
+// : 'http://localhost:4041';
 
 
 export const fetchMasterData = async (endpoint) => {
   try {
-    const response = await axios.get(`${backendUrl}/${endpoint}`);
+    const response = await axios.get(`https://basic-backend-001-fadbheefgmdffzd4.uaenorth-01.azurewebsites.net/organization/${endpoint}`);
     return response.data;
   } catch (error) {
     console.error(`Error fetching ${endpoint} data:`, error);
