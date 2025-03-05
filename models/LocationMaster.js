ea993900c9ae665b17e0ffdd4f28cf00a8bc2ad5
Mansoor <shaikmansoor1200@gmail.com>
2025-03-05 17:18:58 +0530

    replaced master data models from backup

--- models/LocationMaster.js
+++ models/LocationMaster.js
@@ -1,45 +1,11 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const LocationMasterSchema = new Schema({
-    LocationName: {
-        type: String,
-        required: true
-    },
-    TimeZone: {
-        type: String,
-        required: true
-    },
-    CreatedDate: {
-        type: Date,
-        required: true,
-        default: Date.now
-    },
-    CreatedBy: {
-        type: String,
-        required: true
-    },
-    ModifiedDate: {
-        type: Date,
-        default: Date.now
-    },
-    ModifiedBy: {
-        type: String
-    }
-}, { collection: 'LocationMaster' });
-
-
-// Pre-save hook to update ModifiedDate before saving
-LocationMasterSchema.pre('save', function(next) {
-    // Only set CreatedDate if it is a new document
-    if (this.isNew) {
-        this.CreatedDate = Date.now();
-    }
-    // Always update the ModifiedDate
-    this.ModifiedDate = Date.now();
-    next();
+const LocationMasterSchema = new mongoose.Schema({
+  LocationName: { type: String, unique: true, required: true },
+  TimeZone: String,
+  CreatedBy: String,
+  ModifiedBy: String
 });
 
 const LocationMaster = mongoose.model('LocationMaster', LocationMasterSchema);
-
 module.exports = { LocationMaster };
