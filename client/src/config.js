0bdf09a08cd52b0475db70e1dfb102b6b7cbe105
Mansoor <shaikmansoor1200@gmail.com>
2025-03-06 15:39:52 +0530

    removed connectdb

--- client/src/config.js
+++ client/src/config.js
@@ -3,6 +3,5 @@ export const config = {
   REACT_APP_CLIENT_SECRET: "fPhrr3uhTyz4IUhy",
   REACT_APP_REDIRECT_URI: "http://localhost:3000",
   REACT_APP_API_URL: "https://basic-backend-001-fadbheefgmdffzd4.uaenorth-01.azurewebsites.net",
-  // REACT_APP_API_URL: "http://localhost:5000",
   REACT_APP_WS_URL: "ws://localhost:8081"
 };
