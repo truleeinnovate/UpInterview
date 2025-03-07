af81b8f54feaeaac687038b8ce939d637289dbc9
Mansoor <shaikmansoor1200@gmail.com>
2025-03-07 11:53:09 +0530

    added individualLoginController - OutsourceInterviewersSchema, generateOtp - uncommented the CryptoJS, crypto

--- utils/generateOtp.js
+++ utils/generateOtp.js
@@ -1,7 +1,5 @@
-
-
-// const CryptoJS = require("crypto-js");
-// const crypto = require("crypto");
+const CryptoJS = require("crypto-js");
+const crypto = require("crypto");
 
 function generateOTP(candidateId, interval = 180)  {
   try {
@@ -9,15 +7,14 @@ function generateOTP(candidateId, interval = 180)  {
     const secret = `${candidateId}${
       process.env.HMAC_SECRET || "default-secret"
     }`;
-    // const hmac = crypto.createHmac("sha1", secret);
+    const hmac = crypto.createHmac("sha1", secret);
     hmac.update(Buffer.from(time.toString()));
     const hash = hmac.digest("hex");
     const offset = parseInt(hash[hash.length - 1], 16);
     const binary = parseInt(hash.substr(offset * 2, 8), 16) & 0x7fffffff;
 
-    // Generate a 4-digit OTP
     const otp = binary % 10 ** 5;
-    return otp.toString().padStart(5, "0"); // Ensures OTP is always 4 digits
+    return otp.toString().padStart(5, "0");
   } catch (error) {
     console.error("Error generating OTP:", error);
     throw new Error("Unable to generate OTP");
@@ -27,8 +24,8 @@ function generateOTP(candidateId, interval = 180)  {
 
 
 const encrypt = (text, secretKey) => {
-  // const encrypted = CryptoJS.AES.encrypt(text, secretKey).toString();
-  return encodeURIComponent(encrypted); // Make it URL-safe
+  const encrypted = CryptoJS.AES.encrypt(text, secretKey).toString();
+  return encodeURIComponent(encrypted);
 };
 
 
