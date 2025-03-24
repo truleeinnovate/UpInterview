c9b3175f6c741b7cb9779cab492f6b0efce1ba32
Mansoor <shaikmansoor1200@gmail.com>
2025-03-24 16:33:49 +0530

    config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,9 @@
 console.log('process.env.REACT_APP_API_URL frontend api link:', process.env.REACT_APP_API_URL)
-console.log('process.env.REACT_APP_REDIRECT_URI frontend api link:', process.env.REACT_APP_REDIRECT_URI)
 
 export const config = {
   REACT_APP_CLIENT_ID: "86vj2ix33jf4aq",
   REACT_APP_CLIENT_SECRET: "WPL_AP1.dEr0dEfWdtgyK9ER.5Fe8uA==",
-  REACT_APP_REDIRECT_URI: process.env.REACT_APP_REDIRECT_URI,
+  REACT_APP_REDIRECT_URI: "https://frontend-001-c7hzake8ghdbfeeh.canadacentral-01.azurewebsites.net/callback",
   REACT_APP_API_URL: process.env.REACT_APP_API_URL
 };
 
