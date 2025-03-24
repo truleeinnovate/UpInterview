0dc31dbec219a8ffe7ea4cc11c91e29724a39559
Mansoor <shaikmansoor1200@gmail.com>
2025-03-24 16:24:10 +0530

    index.js changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { PermissionsProvider } from './Context/PermissionsContext.js';
 import ReactDOM from 'react-dom/client';
+import App from './App.js';
 import './index.css';
-import App from './App';
-
+import { CustomProvider } from './Context/Contextfetch.js'
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <PermissionsProvider>
+    <Router>
+        <CustomProvider >
+          <Toaster />
+          <App />
+        </CustomProvider>
+    </Router>
+  </PermissionsProvider>
 );
