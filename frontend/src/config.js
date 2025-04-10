1b53c0c1369e166f0e4c84ab0698c8b621c562f8
Mansoor <shaikmansoor1200@gmail.com>
2025-04-10 18:41:29 +0530

    added the redirect url manually in the config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,22 +1,11 @@
 console.log('process.env.REACT_APP_API_URL frontend api link:', process.env.REACT_APP_API_URL)
 console.log('process.env.REACT_APP_REDIRECT_URI frontend api link:', process.env.REACT_APP_REDIRECT_URI)
 
-// Get the current environment
-const isProduction = process.env.NODE_ENV === 'production';
-
 export const config = {
   REACT_APP_CLIENT_ID: "86vj2ix33jf4aq",
   REACT_APP_CLIENT_SECRET: "WPL_AP1.dEr0dEfWdtgyK9ER.5Fe8uA==",
-  // For deployment, use the deployment URL
-  REACT_APP_REDIRECT_URI: isProduction 
-    ? process.env.REACT_APP_REDIRECT_URI || 
-      'https://frontend-001-c7hzake8ghdbfeeh.canadacentral-01.azurewebsites.net/callback'
-    : process.env.REACT_APP_REDIRECT_URI || "http://localhost:3000/callback",
-  // For deployment, use the backend deployment URL
-  REACT_APP_API_URL: isProduction 
-    ? process.env.REACT_APP_API_URL || 
-      'https://backend-001-a6bjhub5amgygjgd.canadacentral-01.azurewebsites.net'
-    : process.env.REACT_APP_API_URL || 'http://localhost:5000'
+  REACT_APP_REDIRECT_URI: process.env.REACT_APP_REDIRECT_URI || "https://frontend-001-c7hzake8ghdbfeeh.canadacentral-01.azurewebsites.net/callback",
+  REACT_APP_API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000'
 };
 
 export const linkedInConfig = {
