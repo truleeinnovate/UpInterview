9c88d1c18b4e4b5691c9f8af9e723af796d1bbb2
Mansoor <shaikmansoor1200@gmail.com>
2025-04-29 17:32:01 +0530

    update index.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    REACT_APP_CLIENT_ID: "86vj2ix33jf4aq",
-    REACT_APP_CLIENT_SECRET: "WPL_AP1.dEr0dEfWdtgyK9ER.5Fe8uA==",
+    REACT_APP_CLIENT_ID: "77eq6sdds9ol1a",
+    REACT_APP_CLIENT_SECRET: "WPL_AP1.rW1TQVlAI93U3fuN.61yPig==",
     REACT_APP_REDIRECT_URI: "https://app.upinterview.io/callback",
 };
