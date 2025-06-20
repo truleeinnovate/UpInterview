64e069902fb8b163dd1ac6cc6e439594d81912c5
Venkatesh <bnvenkatesheee@gmail.com>
2025-06-20 13:31:07 +0530

    Venkatesh : update  in questionbank update lable name getting in myquestions list adding form and suggested questions adding in my question list and also if uncheck means removing also

--- frontend/src/Components/Loading.js
+++ frontend/src/Components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = ({ message = "Loading..." }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+    <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="flex flex-col items-center space-y-4">
         <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-custom-blue"></div>
         <p className="text-center text-gray-600">{message}</p>
