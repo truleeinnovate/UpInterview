3db02d2f19d117bc5aadb9b201859cc3b3570fc2
Mansoor <shaikmansoor1200@gmail.com>
2025-09-06 10:26:40 +0530

    Add PushNotificationControllers and categoryMaster

---
+++ backend/models/MasterSchemas/categoryMaster.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const CategoryMasterSchema = new mongoose.Schema({
+    CategoryName: { type: String, unique: true, required: true, index: true },
+    isActive: { type: Boolean, default: true }
+}, {
+    timestamps: true,
+    collection: 'CategoryMaster'
+});
+
+const CategoryMaster = mongoose.model('CategoryMaster', CategoryMasterSchema);
+module.exports = { CategoryMaster };
