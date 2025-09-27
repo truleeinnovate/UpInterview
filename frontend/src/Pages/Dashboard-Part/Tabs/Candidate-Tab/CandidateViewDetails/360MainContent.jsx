ffea57af2591c7d9c17018f850e1bdd88342e8b5
Ashok Dev <ashokchodipalli1@gmail.com>
2025-09-27 18:36:07 +0530

    Ashok: fixed style issues and changed ui of basic details at assessment template view and changed icons at candidate 360 view

--- frontend/src/Pages/Dashboard-Part/Tabs/Candidate-Tab/CandidateViewDetails/360MainContent.jsx
+++ frontend/src/Pages/Dashboard-Part/Tabs/Candidate-Tab/CandidateViewDetails/360MainContent.jsx
@@ -1,42 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import InterviewRounds from './InterviewRounds';
-import AppliedPositions from './AppliedPositions';
-import Timeline from './Timeline';
-import PositionDetails from './PositionDetails';
-import InterviewDetails from './InterviewDetails';
-import classNames from 'classnames';
-import Sidebar from './Sidebar';
-import { Outlet, useParams } from 'react-router-dom';
-import axios from 'axios';
-import AddCandidateForm from '../AddCandidateForm';
-import { User, } from 'lucide-react';
-import { useInterviews } from '../../../../../apiHooks/useInterviews.js';
-import Documents from './Documents';
-import Loading from '../../../../../Components/Loading';
-import Activity from '../../CommonCode-AllTabs/Activity';
-import { config } from '../../../../../config';
+// v1.0.0 - Ashok - Changed icons
+
+import React, { useEffect, useState } from "react";
+import InterviewRounds from "./InterviewRounds";
+import AppliedPositions from "./AppliedPositions";
+import Timeline from "./Timeline";
+import PositionDetails from "./PositionDetails";
+import InterviewDetails from "./InterviewDetails";
+import classNames from "classnames";
+import Sidebar from "./Sidebar";
+import { Outlet, useParams } from "react-router-dom";
+import axios from "axios";
+import AddCandidateForm from "../AddCandidateForm";
+// v1.0.0 <--------------------------------------------------------
+import {
+  User,
+  Users,
+  Briefcase,
+  Calendar,
+  FileText,
+  Activity,
+} from "lucide-react";
+// v1.0.0 -------------------------------------------------------->
+import { useInterviews } from "../../../../../apiHooks/useInterviews.js";
+import Documents from "./Documents";
+import Loading from "../../../../../Components/Loading";
+// v1.0.0 <--------------------------------------------------------
+import ActivityComponent from "../../CommonCode-AllTabs/Activity";
+// v1.0.0 -------------------------------------------------------->
+import { config } from "../../../../../config";
+
+// v1.0.0 <--------------------------------------------------------
+// const tabs = [
+//   { id: "interviews", name: "Interviews", icon: "👥" },
+//   { id: "positions", name: "Positions", icon: "💼" },
+//   { id: "timeline", name: "Timeline", icon: "📅" },
+//   { id: "documents", name: "Documents", icon: "📄" },
+//   { id: "Activity", name: "Activity", icon: "📊" },
+// ];
 
 const tabs = [
-  { id: 'interviews', name: 'Interviews', icon: '👥' },
-  { id: 'positions', name: 'Positions', icon: '💼' },
-  { id: 'timeline', name: 'Timeline', icon: '📅' },
-  { id: 'documents', name: 'Documents', icon: '📄' },
-  { id: 'Activity', name: 'Activity', icon: '📊' }
+  { id: "interviews", name: "Interviews", icon: Users },
+  { id: "positions", name: "Positions", icon: Briefcase },
+  { id: "timeline", name: "Timeline", icon: Calendar },
+  { id: "documents", name: "Documents", icon: FileText },
+  { id: "activity", name: "Activity", icon: Activity },
 ];
+// v1.0.0 -------------------------------------------------------->
 
 const MainContent = () => {
-
-  const [activeTab, setActiveTab] = useState('interviews');
+  const [activeTab, setActiveTab] = useState("interviews");
   const [selectedPosition, setSelectedPosition] = useState(null);
   const [selectedInterview, setSelectedInterview] = useState(null);
   const { id } = useParams();
   const [candidate, setCandidate] = useState(null);
   const [positions, setPositions] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [showAddForm, setShowAddForm] = useState(false);
   const [editModeOn, setEditModeOn] = useState(false);
   const [slideShow, setSlideShow] = useState(false);
-
 
   console.log("candidate 360 Details", candidate);
   console.log("positions 360 Details", positions);
@@ -45,9 +66,7 @@ const MainContent = () => {
   //   interviewData
   // } = useCustomContext();
 
-  const {
-    interviewData,
-  } = useInterviews();
+  const { interviewData } = useInterviews();
 
   const interview = interviewData.find((data) => data?.candidateId?._id === id);
 
@@ -57,22 +76,21 @@ const MainContent = () => {
 
   const fetchCandidate = async () => {
     try {
-      const response = await axios.get(`${config.REACT_APP_API_URL}/candidate/${id}`);
+      const response = await axios.get(
+        `${config.REACT_APP_API_URL}/candidate/${id}`
+      );
       const { appliedPositions, ...candidateData } = response.data;
       setCandidate(candidateData);
       setPositions(appliedPositions || []);
-
     } catch (error) {
-      console.error('Error fetching candidate:', error);
+      console.error("Error fetching candidate:", error);
     }
   };
 
-
   // if (!candidate) return <Loading />
 
-
   const handleViewInterview = (interviewId) => {
-    const interview = candidate?.interviews?.find(i => i.id === interviewId);
+    const interview = candidate?.interviews?.find((i) => i.id === interviewId);
     if (interview) {
       setSelectedInterview(interview);
     }
@@ -97,7 +115,6 @@ const MainContent = () => {
     //     isOpen={slideShow || window.innerWidth >= 1023}
     //     onClose={() => setSlideShow(false)}
     //   />
-
 
     //   <main className="flex-1 overflow-y-auto md:pt-3 h-full md:h-screen lg:h-screen xl:h-screen 2xl:h-screen bg-gray-50 md:w-[100%] lg:w-[75%] xl:w-[75%] 2xl:w-[75%]">
     //     <div className="w-full mx-auto px-4 sm:px-4 py-4 sm:py-6">
@@ -149,7 +166,7 @@ const MainContent = () => {
     //           {activeTab === 'interviews' && (
     //             <>
     //               <InterviewRounds
-    //                 // interview 
+    //                 // interview
     //                 interviews={interview || []}
     //                 onViewDetails={setSelectedInterview}
     //                 onEdit={handleEdit}
@@ -214,7 +231,6 @@ const MainContent = () => {
 
     // </div>
 
-
     <div className="flex flex-col lg:flex-row xl:flex-row 2xl:flex-row min-h-screen bg-gray-50 sm:mt-0 lg:mt-3 xl:mt-3 2xl:mt-3">
       <Sidebar
         candidate={candidate}
@@ -234,13 +250,16 @@ const MainContent = () => {
                   key={tab.id}
                   onClick={() => setActiveTab(tab.id)}
                   className={classNames(
-                    'px-4 py-2.5 mr-2 font-medium transition-all duration-200 flex items-center gap-1.5 whitespace-nowrap text-sm',
+                    "px-4 py-2.5 mr-2 font-medium transition-all duration-200 flex items-center gap-1.5 whitespace-nowrap text-sm",
                     activeTab === tab.id
-                      ? 'border-b-2 border-custom-blue text-custom-blue'
-                      : 'text-gray-600 rounded-lg hover:bg-gray-100 shadow-sm'
+                      ? "border-b-2 border-custom-blue text-custom-blue"
+                      : "text-gray-600 rounded-lg hover:bg-gray-100 shadow-sm"
                   )}
                 >
-                  <span className="text-base">{tab.icon}</span>
+                  {/* v1.0.0 <------------------------------------------------------- */}
+                  {/* <span className="text-base">{tab.icon}</span> */}
+                  <tab.icon className="w-5 h-5" />
+                  {/* v1.0.0 -------------------------------------------------------> */}
                   <span>{tab.name}</span>
                 </button>
               ))}
@@ -253,7 +272,7 @@ const MainContent = () => {
             {/* Content Section */}
             <div className="bg-white rounded-lg shadow-sm border border-gray-100 p-3 sm:p-4 md:p-6 h-[calc(100vh-200px)] sm:h-auto overflow-y-auto">
               <div className="relative">
-                {activeTab === 'interviews' && (
+                {activeTab === "interviews" && (
                   <>
                     <InterviewRounds
                       interviews={interview || []}
@@ -270,7 +289,7 @@ const MainContent = () => {
                     )}
                   </>
                 )}
-                {activeTab === 'positions' && (
+                {activeTab === "positions" && (
                   <>
                     <AppliedPositions
                       positions={positions || []}
@@ -284,18 +303,20 @@ const MainContent = () => {
                     )}
                   </>
                 )}
-                {activeTab === 'timeline' && (
+                {activeTab === "timeline" && (
                   <Timeline
                     events={candidate.timeline || []}
                     onViewInterview={handleViewInterview}
                   />
                 )}
-                {activeTab === 'documents' && (
+                {activeTab === "documents" && (
                   <Documents documents={candidate.documents || []} />
                 )}
-                {activeTab === 'Activity' && (
-                  <Activity parentId={id} />
+                {/* v1.0.0 <---------------------------------------------------- */}
+                {activeTab === "Activity" && (
+                  <ActivityComponent parentId={id} />
                 )}
+                {/* v1.0.0 <----------------------------------------------------> */}
               </div>
             </div>
           </div>
