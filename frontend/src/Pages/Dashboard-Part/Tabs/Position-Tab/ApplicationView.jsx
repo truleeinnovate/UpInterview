899edaa06575de8cc21b2549aada9fbaa0311bc8
TruleeInnovate <upinterview8@gmail.com>
2026-01-28 00:51:55 +0530

    azure index issues solved

--- frontend/src/Pages/Dashboard-Part/Tabs/Position-Tab/ApplicationView.jsx
+++ frontend/src/Pages/Dashboard-Part/Tabs/Position-Tab/ApplicationView.jsx
@@ -28,6 +28,8 @@ export default function ApplicationView({ application, onBack }) {
     const [showAllResumes, setShowAllResumes] = useState(false);
     const [showUploadResume, setShowUploadResume] = useState(false);
     const candidateId = application.candidateId?._id || application.candidateId;
+    console.log("application",application);
+    
     console.log(candidateId);
     const { candidate: fetchedCandidate, isLoading, refetch } = useCandidateById(candidateId);
     const { data: stats } = useCandidateStats(candidateId);
