8b87ade1558f6603e2279638b279475293e7969a
Ashok Dev <ashokchodipalli1@gmail.com>
2026-02-04 11:49:38 +0530

    Ashok: Internal Interviews searchbar fixed width

--- frontend/src/Pages/Dashboard-Part/Tabs/Interview-New/pages/Internal-Or-Outsource/InternalInterviewers.jsx
+++ frontend/src/Pages/Dashboard-Part/Tabs/Interview-New/pages/Internal-Or-Outsource/InternalInterviewers.jsx
@@ -533,16 +533,16 @@ const InternalInterviews = ({
         {/* Filter Toggle Button with Arrow */}
         {navigatedfrom !== "dashboard" && (
           <>
-            <div className="w-full flex justify-end items-center gap-2 mt-4">
+            <div className="w-full flex items-center gap-2 mt-4">
               {/* RIGHT SECTION — SEARCH */}
-              <div className="relative">
+              <div className="relative w-full">
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
                 <input
                   type="text"
                   placeholder="Search individuals..."
                   value={searchQuery}
                   onChange={handleSearchInputChange}
-                  className="w-full pl-10 px-2 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                  className="w-full pl-10 px-2 h-10 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 />
               </div>
               <button
